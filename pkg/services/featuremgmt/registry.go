eb51188cf18417300a304bfa11d4f2f2b4f2f82e
Jack Westbrook <jack.westbrook@gmail.com>
2025-02-19 17:04:57 +0100

    feat(featuremgmt): introduce feature toggle for enabling sri checks

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1789,6 +1789,13 @@ var (
 			HideFromAdminPage: true,
 			HideFromDocs:      true,
 		},
+		{
+			Name:         "assetSriChecks",
+			Description:  "Enables SRI checks for grafana javascript assets",
+			Stage:        FeatureStageExperimental,
+			Owner:        grafanaPluginsPlatformSquad,
+			FrontendOnly: true,
+		},
 	}
 )
 
