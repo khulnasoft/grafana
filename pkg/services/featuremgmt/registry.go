0962a25f81f49c191b5c4de7067f741b2d375eee
Jack Westbrook <jack.westbrook@gmail.com>
2025-02-19 17:58:49 +0100

    docs(featuremgmt): update assetSriChecks to pass CI

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -1791,7 +1791,7 @@ var (
 		},
 		{
 			Name:         "assetSriChecks",
-			Description:  "Enables SRI checks for grafana javascript assets",
+			Description:  "Enables SRI checks for Grafana Javascript assets",
 			Stage:        FeatureStageExperimental,
 			Owner:        grafanaPluginsPlatformSquad,
 			FrontendOnly: true,
